fc76333a0702e6beb460331a1356c3f18d25b8a3
jwill119 <jwill119@uwyo.edu>
2018-12-03 15:35:53 -0700

    Did some work

--- main.cpp
+++ main.cpp
@@ -6,3 +6,48 @@
 
 // The map sorter main file.
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+using namespace std;
+
+// The functions
+
+// Reads the functions into a map, counting the number of times they appear.
+map<string,int> readIn(string filename) {
+    ifstream theFile;
+    theFile.open(filename);
+    string word;
+    word.clear();
+
+    map<string,int> theCount;
+
+    while (theFile >> word) {
+        ++theCount[word];
+    }
+
+    theFile.close();
+
+    return theCount;
+}
+
+// Writes a tally-map to a text file.
+void writeOut(map<string,int> theCount) {
+    ofstream outFile("output.txt");
+    for(auto& kv : theCount) {
+        outFile << theCount.second;
+    }
+
+    outFile.close();
+    return;
+}
+
+int main() {
+
+    cout << "Reading color words from 'example.txt'." << endl << endl;
+    map<string,int> counts = readIn("example.txt");
+    writeOut(counts);
+
+    return 0;
+}
